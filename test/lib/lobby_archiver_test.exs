1256b1e45f93995757e3f77fd0eac29b804d9fd9
Alex Tuktarov <sanchos.1517@gmail.com>
2019-03-29 16:51:44 +0300

    add lobby_archiver test

--- test/lib/lobby_archiver_test.exs
+++ test/lib/lobby_archiver_test.exs
@@ -1,8 +1,17 @@
-# defmodule PhoenixSeaBattle.LobbyArchiverTest do
-#   use ExUnit.Case
-#   alias PhoenixSeaBattle.LobbyArchiver
-#   require Logger
+defmodule PhoenixSeaBattle.LobbyArchiverTest do
+  use ExUnit.Case
+  alias PhoenixSeaBattle.LobbyArchiver
+  require Logger
 
-#   test ":timeout", do: assert send(LobbyArchiver, :timeout)
-#   test "some msg", do: assert send(LobbyArchiver, "some msg")
-# end
+  test "subs/0 and new_msg/2" do
+    LobbyArchiver.subs()
+    body = "hello world"
+    user = "SomeUser"
+    LobbyArchiver.new_msg(body, user)
+    [%{body: ^body, user: ^user, timestamp: _}] = LobbyArchiver.get_messages()
+    send LobbyArchiver, :timeout
+    [%{body: ^body, user: ^user, timestamp: _}] = LobbyArchiver.get_messages()
+
+    assert_receive {:update, [%{body: ^body, user: ^user, timestamp: _}]}
+  end
+end
