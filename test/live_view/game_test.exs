6f2b1c22f6a94bbc11a077ca20fff6ea8a6b882d
александр туктаров <alexandr.tutkarev@blockchain.ru>
2019-04-08 23:22:54 +0300

    up phoenix_live_view, up test

--- test/live_view/game_test.exs
+++ test/live_view/game_test.exs
@@ -15,23 +15,19 @@ defmodule PhoenixSeaBattleWeb.GameTest do
   end
 
   test "add admin, opponent, third player can't connect", %{user1: user1, user2: user2, id: id} do
-    {:ok, :admin} = GameServer.add_user(GameSupervisor.via_tuple(id), user1)
     token = Phoenix.Controller.get_csrf_token()
-    params = [session: %{user: user1, id: id, token: token}]
 
-    {:ok, view, _html} = LiveViewTest.mount(Endpoint, GameLive, params)
-    "" <> _ = LiveViewTest.render(view)
+    {:ok, :admin} = GameServer.add_user(GameSupervisor.via_tuple(id), user1)
+    params = [session: %{user: user1, id: id, token: token}]
+    {:ok, %LiveViewTest.View{}, _} = LiveViewTest.mount(Endpoint, GameLive, params)
 
     {:ok, :opponent} = GameServer.add_user(GameSupervisor.via_tuple(id), user2)
     params = [session: %{user: user2, id: id, token: token}]
-
-    {:ok, view, _html} = LiveViewTest.mount(Endpoint, GameLive, params)
-    "" <> _ = LiveViewTest.render(view)
+    {:ok, %LiveViewTest.View{}, _} = LiveViewTest.mount(Endpoint, GameLive, params)
 
     user3 = insert_user(%{name: "user3"})
     {:error, "game already full"} = GameServer.add_user(GameSupervisor.via_tuple(id), user3)
     params = [session: %{user: user3, id: id, token: token}]
-
     # User will be redirected to GET /game/:id
     path = "/game/#{id}"
     assert {:error, %{redirect: ^path}} = LiveViewTest.mount(Endpoint, GameLive, params)
