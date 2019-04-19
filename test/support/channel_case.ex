b5138dadfdaf134f30c0b6f5bdd1e3fc20a4e938
Alex Tuktarov <sanchos.1517@gmail.com>
2019-04-19 18:49:44 +0300

    switch off channel_case

--- test/support/channel_case.ex
+++ test/support/channel_case.ex
@@ -1,43 +1,43 @@
-defmodule PhoenixSeaBattleWeb.ChannelCase do
-  @moduledoc """
-  This module defines the test case to be used by
-  channel tests.
+# defmodule PhoenixSeaBattleWeb.ChannelCase do
+#   @moduledoc """
+#   This module defines the test case to be used by
+#   channel tests.
 
-  Such tests rely on `Phoenix.ChannelTest` and also
-  import other functionality to make it easier
-  to build and query models.
+#   Such tests rely on `Phoenix.ChannelTest` and also
+#   import other functionality to make it easier
+#   to build and query models.
 
-  Finally, if the test case interacts with the database,
-  it cannot be async. For this reason, every test runs
-  inside a transaction which is reset at the beginning
-  of the test unless the test case is marked as async.
-  """
+#   Finally, if the test case interacts with the database,
+#   it cannot be async. For this reason, every test runs
+#   inside a transaction which is reset at the beginning
+#   of the test unless the test case is marked as async.
+#   """
 
-  use ExUnit.CaseTemplate
+#   use ExUnit.CaseTemplate
 
-  using do
-    quote do
-      # Import conveniences for testing with channels
-      use Phoenix.ChannelTest
+#   using do
+#     quote do
+#       # Import conveniences for testing with channels
+#       use Phoenix.ChannelTest
 
-      alias PhoenixSeaBattle.Repo
-      import Ecto
-      import Ecto.Changeset
-      import Ecto.Query
-      import PhoenixSeaBattleWeb.TestHelpers
+#       alias PhoenixSeaBattle.Repo
+#       import Ecto
+#       import Ecto.Changeset
+#       import Ecto.Query
+#       import PhoenixSeaBattleWeb.TestHelpers
 
-      # The default endpoint for testing
-      @endpoint PhoenixSeaBattleWeb.Endpoint
-    end
-  end
+#       # The default endpoint for testing
+#       @endpoint PhoenixSeaBattleWeb.Endpoint
+#     end
+#   end
 
-  setup tags do
-    :ok = Ecto.Adapters.SQL.Sandbox.checkout(PhoenixSeaBattle.Repo)
+#   setup tags do
+#     :ok = Ecto.Adapters.SQL.Sandbox.checkout(PhoenixSeaBattle.Repo)
 
-    unless tags[:async] do
-      Ecto.Adapters.SQL.Sandbox.mode(PhoenixSeaBattle.Repo, {:shared, self()})
-    end
+#     unless tags[:async] do
+#       Ecto.Adapters.SQL.Sandbox.mode(PhoenixSeaBattle.Repo, {:shared, self()})
+#     end
 
-    :ok
-  end
-end
+#     :ok
+#   end
+# end
